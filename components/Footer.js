c39b2ae77a5cf2b0cf91e7082e5017c113e9bdc4
Sindre <sinfr91@gmail.com>
2022-05-22 13:06:13 +1200

    add icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,11 @@
 function Footer() {
   return (
-    <div>
+    <div className="flex flex-1">
       <a href="https://www.github.com">
-        <img src="/vercel.svg"></img>
+        <img src="/github.png"></img>
+      </a>
+      <a href="mailto:sinfr91@gmail.com">
+        <img src="/mail.png"></img>
       </a>
       <span>All rights reserved @ Sindre 2022</span>
     </div>
--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,11 @@
 function Header() {
   return (
-    <nav>
-      <a>Home</a>
-      <a>About</a>
-      <a>Projects</a>
+    <nav className="hover:bg-gray-200">
+      <div className="flex justify-between">
+        <a>Home</a>
+        <a>About</a>
+        <a>Projects</a>
+      </div>
     </nav>
   )
 }
