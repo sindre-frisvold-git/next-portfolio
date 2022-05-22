86aab2e0fb3f964531f2a57d5ddacac0b60b13eb
Sindre <sinfr91@gmail.com>
2022-05-22 17:21:02 +1200

    add title

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="flex flex-1">
+    <div className="bottom-0 absolute flex flex-1">
       <a href="https://www.github.com">
         <img src="/github.png"></img>
       </a>
