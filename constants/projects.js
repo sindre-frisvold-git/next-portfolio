92dccd7e351337c3b63314be31d4e869d04547eb
Sindre <sinfr91@gmail.com>
2022-05-22 23:40:02 +1200

    add constants

---
+++ constants/projects.js
@@ -0,0 +1,15 @@
+const projects = [
+  {
+    title: 'Calculator',
+    link: 'https://sindre-frisvold-git.github.io/calculator/',
+  },
+  {
+    title: 'PlanBuddy, FInal Project',
+    link: 'https://github.com/sindre-frisvold-git/planbuddy',
+  },
+  {
+    title: 'Etch-A-Skecth',
+    link: 'https://sindre-frisvold-git.github.io/etch-a-sketch/',
+  },
+]
+export default projects
