3bc0588abad895843e756af0072d97d4a6e2810c
Sindre <sinfr91@gmail.com>
2022-05-24 22:28:10 +1200

    add svg

--- components/Projects.js
+++ components/Projects.js
@@ -4,7 +4,7 @@ function Projects() {
   return (
     <div className="overflow-y-scroll max-h-80 gap-y-11">
       {projects.map((el) => {
-        return <ProjectCard key={el.title} title={el.title} />
+        return <ProjectCard key={el.title} title={el.title} tech={el.tech} />
       })}
     </div>
   )
--- constants/projects.js
+++ constants/projects.js
@@ -1,3 +1,5 @@
+import { css3, html5, nodeJs, react, redux, sass } from '../utils/svg'
+
 const projects = [
   {
     title: 'Calculator',
@@ -6,6 +8,7 @@ const projects = [
   {
     title: 'PlanBuddy, FInal Project',
     link: 'https://github.com/sindre-frisvold-git/planbuddy',
+    tech: [react, redux, sass, html5, css3, nodeJs],
   },
   {
     title: 'Etch-A-Skecth',
