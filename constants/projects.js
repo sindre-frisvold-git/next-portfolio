c0634381ceb636269a5a247afb4e5e8340b14803
Sindre <sinfr91@gmail.com>
2022-05-26 11:33:53 +1200

    add project descriptions

--- constants/projects.js
+++ constants/projects.js
@@ -20,20 +20,36 @@ const projects = [
     tech: [css3, javaScript, html5],
   },
   {
-    title: 'Etch-A-Sketch',
-    img: '/etch.png',
-    link: 'https://sindre-frisvold-git.github.io/etch-a-sketch/',
-    repo: 'https://github.com/sindre-frisvold-git/etch-a-sketch',
-    description: 'An experiment in DOM manipulation and responsive design.',
-    tech: [css3, javaScript, html5],
-  },
-  {
     title: 'PlanBuddy',
     img: '/planBuddy.png',
     link: 'https://github.com/sindre-frisvold-git/planbuddy',
     repo: 'https://github.com/sindre-frisvold-git/planbuddy',
     description: 'A full-stack app to help you plan your learning.',
+    para: [
+      'I created this with my team as our final project at Dev Academy. We wanted to create a platform where you could log in and easily create a learning plan with byte-sized tasks within subgoals.',
+      'We started by planning out the basic functionality then we wireframes and made a flow-chart for the entire app. We spent almost 2 days doing the planning which paid off considerably during the coding stage. We maintained an Agile development flow with daily scheduled stand-ups and check-ins.',
+      'As Agile Lead I took into account the retros from previous projects and ensured we implemented a solid wellbeing plan and used time-boxing to keep everyone on track during planning and feature-development. I also assessed each team member’s strengths at the start of the project so we were all able to continuously perform at our peak performance.',
+      'In the end we produced a functioning full-stack app which had all the functionality we initially wanted.',
+    ],
+
     tech: [react, redux, sass, html5, javaScript],
+  },
+  {
+    title: 'Etch-A-Sketch',
+    img: '/etch.png',
+    link: 'https://sindre-frisvold-git.github.io/etch-a-sketch/',
+    repo: 'https://github.com/sindre-frisvold-git/etch-a-sketch',
+    description: 'An experiment in DOM manipulation and responsive design.',
+    para: [
+      'This project was initially built to experiment with scalable DOM manipulation by creating a “screen” given a number N for “pixels”.',
+    ],
+    challenge: [
+      'There were a few major challenges to overcome with this project:',
+      'The mouseover event used to color the “pixels” did not work on mobile for obvious reasons. Furthermore, the touchmove event I tried using as a fix did not register the “pixels” I dragged across. To fix this I had to learn how to drill into the event object where I eventually found “clientX” and “clientY”. I used this together with “elementFromPoint” to replicate the mouseover functionality for touch screens.',
+      'I wanted to have “knobs” turning based on your interaction with the etcher, for this I needed the page to know which direction you entered the “pixel” from. With a bit of tinkering I wrote some logic that calculated the direction of entry based on the previous “pxel” with the x and y coordinate and compared to the “pixel” entered. I also had to write some code to account for “pixel” size for the amount of rotation of the “knobs”.',
+      'In the end I managed to create the functionality I was looking for while keeping the interface simple. I learned a lot about how to use DOM events which has allowed me to create more responsive and interactive code thereforth.',
+    ],
+    tech: [css3, javaScript, html5],
   },
   {
     title: 'Calculator',
