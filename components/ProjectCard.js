314fb35e261f19886d65fedd8048aa06c32503f0
Sindre <sinfr91@gmail.com>
2022-05-25 17:03:12 +1200

    remove drag scroll

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -3,9 +3,10 @@ import Link from 'next/link'
 export default function ProjectCard({ title, tech }) {
   return (
     // <Link>
-    <div className="h-60 w-full relative rounded-md my-1 md:w-3/5 bg-red-100 dark:bg-gray-700 overflow-hidden scroll-mt-9 mt-9 mb-9 snap-child cursor-grabbing">
+    <div className="h-60 w-1 relative rounded-md my-1 md:w-3/5 bg-red-100 dark:bg-gray-700 overflow-hidden scroll-mt-9 mt-9 mb-9 snap-child cursor-ns-resize">
       <div className="absolute h-full w-full">
         <img
+          draggable="false"
           className="h-full w-full"
           src="/github.png"
           alt="project image"
--- components/Projects.js
+++ components/Projects.js
@@ -1,12 +1,52 @@
+import { motion } from 'framer-motion'
+import { useRef } from 'react'
 import projects from '../constants/projects'
 import ProjectCard from './ProjectCard'
 function Projects() {
+  // const projectScroll = useRef(null)
+  // let pos = { top: 0, left: 0, x: 0, y: 0 }
+  // console.log(projectScroll)
+  // const mouseDownHandler = function (e) {
+  //   pos = {
+  //     // The current scroll
+  //     // left: projectScroll.current.scrollLeft,
+  //     top: projectScroll.current.scrollTop,
+  //     // Get the current mouse position
+  //     // x: e.clientX,
+  //     y: e.clientY,
+  //   }
+  //   document.addEventListener('mousemove', mouseMoveHandler)
+  //   document.addEventListener('mouseup', mouseUpHandler)
+  // }
+  // const mouseUpHandler = function () {
+  //   document.removeEventListener('mousemove', mouseMoveHandler)
+  //   document.removeEventListener('mouseup', mouseUpHandler)
+  //   console.log('yas')
+  // }
+  // const mouseMoveHandler = function (e) {
+  //   // How far the mouse has been moved
+  //   // const dx = e.clientX - pos.x
+  //   const dy = e.clientY - pos.y
+
+  //   // Scroll the element
+  //   projectScroll.current.scrollTop = pos.top - dy
+  //   // console.log(pos.top)
+  //   // console.log(e.clientY)
+  //   // projectScroll.scrollLeft = pos.left - dx
+  // }
+
   return (
-    <div className="overflow-y-scroll max-h-96 snap-parent scroll-smooth ">
-      {projects.map((el) => {
-        return <ProjectCard key={el.title} title={el.title} tech={el.tech} />
-      })}
-    </div>
+    <motion.div>
+      <div
+        onMouseDown={mouseDownHandler}
+        ref={projectScroll}
+        className="overflow-y-scroll max-h-96 snap-parent scroll-smooth "
+      >
+        {projects.map((el) => {
+          return <ProjectCard key={el.title} title={el.title} tech={el.tech} />
+        })}
+      </div>
+    </motion.div>
   )
 }
 
