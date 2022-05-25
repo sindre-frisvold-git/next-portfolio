67ca78298c4309a034fd21af16cc619c658514f9
Sindre <sinfr91@gmail.com>
2022-05-25 18:04:10 +1200

    add scroll butons

--- components/Projects.js
+++ components/Projects.js
@@ -3,7 +3,7 @@ import { useRef } from 'react'
 import projects from '../constants/projects'
 import ProjectCard from './ProjectCard'
 function Projects() {
-  // const projectScroll = useRef(null)
+  const projectScroll = useRef(null)
   // let pos = { top: 0, left: 0, x: 0, y: 0 }
   // console.log(projectScroll)
   // const mouseDownHandler = function (e) {
@@ -34,19 +34,40 @@ function Projects() {
   //   // console.log(e.clientY)
   //   // projectScroll.scrollLeft = pos.left - dx
   // }
-
+  function scrollUp() {
+    projectScroll.current.scrollTop -= 150
+    console.log(projectScroll.current.scrollTop)
+  }
+  function scrollDown() {
+    projectScroll.current.scrollTop += 150
+    console.log(projectScroll.current.scrollTop)
+  }
   return (
-    <motion.div>
+    <div className="md:flex justify-center">
       <div
         // onMouseDown={mouseDownHandler}
-        // ref={projectScroll}
+        ref={projectScroll}
         className="overflow-y-scroll max-h-96 snap-parent scroll-smooth "
       >
         {projects.map((el) => {
           return <ProjectCard key={el.title} title={el.title} tech={el.tech} />
         })}
       </div>
-    </motion.div>
+      <div className="hidden md:flex md:flex-col justify-center gap-y-5 ml-2">
+        <button
+          className="flex justify-center w-5 h-10 text-3xl text-gray-800 rounded-md bg-sky-300 dark:bg-green-400 hover:bg-sky-400 dark:hover:bg-green-300"
+          onClick={scrollUp}
+        >
+          <div>&uarr;</div>
+        </button>
+        <button
+          className="flex justify-center w-5 h-10 text-3xl text-gray-800 rounded-md bg-sky-300 dark:bg-green-400 hover:bg-sky-400 dark:hover:bg-green-300"
+          onClick={scrollDown}
+        >
+          <div>&darr;</div>
+        </button>
+      </div>
+    </div>
   )
 }
 
