30bbab606862a4d4e306f67eaf3913b394f0f339
Sindre <sinfr91@gmail.com>
2022-06-02 16:16:37 +1200

    add interactivity to elements

--- components/Header.js
+++ components/Header.js
@@ -6,21 +6,23 @@ function Header() {
     e.preventDefault()
     document.documentElement.classList.toggle('dark')
   }
+  const linkStyle =
+    'hover:text-sky-500 hover:dark:text-green-400 transition duration-200'
   return (
     <nav className="pt-3">
       <div className="flex justify-end gap-10 mr-10 text-l md:text-3xl">
         {darkMode(
-          'w-5 md:w-7 transition duration-200 fill-gray-800 dark:fill-slate-100 cursor-pointer',
+          'w-5 md:w-7 transition duration-200 fill-gray-800 dark:fill-slate-100 hover:fill-sky-500 hover:dark:fill-green-400 cursor-pointer',
           clickHandler
         )}
         <Link href="/projects">
-          <a>Projects</a>
+          <a className={linkStyle}>Projects</a>
         </Link>
         <Link href="/about">
-          <a>About</a>
+          <a className={linkStyle}>About</a>
         </Link>
         <Link href="/">
-          <a>Home</a>
+          <a className={linkStyle}>Home</a>
         </Link>
       </div>
     </nav>
